90772c7d52a45b902cb0f6b3e85c050b23dea55a
t190014 <t190014@mail.ryukoku.ac.jp>
2020-05-11 16:42:28 +0000

    提出

--- kadai5-2.c
+++ kadai5-2.c
@@ -2,5 +2,13 @@
 #include <math.h>
 
 int main() {
-  printf()
+    double a, b;
+    a = 1.0;
+    b = 0.0;
+    while(a > b && a < INFINITY) {
+        b = a;
+        a *= 2;
+    }
+    printf("%e\n", b);
+    return 0;
 }
